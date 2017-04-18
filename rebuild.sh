d0e7daa1b9148d66c7ee8fa620aa90e31fb65e5f
Andre Klärner <kandre@ak-online.be>
2017-04-18 20:15:11 +0200

    redo everything with reprepro

--- rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-apt-ftparchive packages . > Packages
-gzip -9 <Packages >Packages.gz
-apt-ftparchive release . >Release
-gpg --armor --sign --detach-sign --output - Release >Release.gpg
-git add .
-git commit -m 'update repository and regenerate Packages.gz'
-git push
