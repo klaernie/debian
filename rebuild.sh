83eaef1654c064b2d143727051004f7f2fceccb0
Andre Klärner <kandre@ak-online.be>
2017-04-18 19:37:10 +0200

    rebuild rebuilding

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-dpkg-scanpackages -m . | gzip --fast > Packages.gz
+apt-ftparchive packages . | gzip --fast > Packages.gz
+apt-ftparchive release . >Release
+gpg --armor --sign --detach-sign Release
 git add .
 git commit -m 'update repository and regenerate Packages.gz'
 git push
