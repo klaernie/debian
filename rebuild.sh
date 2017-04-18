e3aa9c3d95f2ab5d23fad66436fe5877dd294fd1
Andre Klärner <kandre@ak-online.be>
2017-04-18 19:40:20 +0200

    update repository and regenerate Packages.gz

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 apt-ftparchive packages . | gzip --fast > Packages.gz
 apt-ftparchive release . >Release
-gpg --armor --sign --detach-sign Release
+gpg --armor --sign --detach-sign --output Release.gpg Release
 git add .
 git commit -m 'update repository and regenerate Packages.gz'
 git push
